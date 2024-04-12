ec11c48919836ebff794c302fc94c295f7e8ac02
Steve Tinney <stinney@upenn.edu>
2024-04-12 11:49:15 +0000

    glyphs in rev-add

--- reboot/bin/revtabs.sh
+++ reboot/bin/revtabs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 #
 # ADD
-grep add revise.tab | cut -f1,3,10,14,5 | rocox -C 12453 >rev-add.tab
+grep add revise.tab | cut -f1,3,8,10,14 | rocox -C14235 >rev-add.tab
 # DELETE
 grep delete revise.tab | cut -f1,5,9,14 | rocox -C 1342 >rev-delete.tab
 # IGNORE
@@ -12,5 +12,9 @@ grep newglyph revise.tab | cut -f1,3,5,10,14 >rev-newglyph.tab
 # REMOVE
 grep remove revise.tab | cut -f1,9,14 >rev-remove.tab
 # RENAME
-grep rename revise.tab | cut -f1,4,9,10,14 | \
-    sed 's/PROTO-CUNEIFORM SIGN //g' >rev-rename.tab
+grep rename revise.tab | cut -f1,4,9,10,14 >rev-rename.tab
+rm -fr revtabs
+mkdir -p revtabs
+for a in rev-*.tab ; do
+    sed 's/PROTO-CUNEIFORM SIGN //g' <$a >revtabs/$a
+done
