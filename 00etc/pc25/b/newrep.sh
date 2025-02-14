920fb0fa138d881c388b2204c7929ca48121a696
Steve Tinney <stinney@upenn.edu>
2025-02-14 13:31:33 +0000

    pc25 doco portal build draft overall complete but lots of details to fix

---
+++ 00etc/pc25/b/newrep.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# Build a list of OIDs in correct sort sequence to define the PC25 repertoire
+#
+
+# Work in tmp dir
+mkdir -p t
+
+# Create a list of OIDs to be removed
+cut -f1 rm-nc-heads.tsv >t/rem
+cat rm-broken.txt rm-opaque.txt rm-seq.txt >>t/rem
+
+# Get ap24's OIDs
+cut -f1 ../ap24-repertoire.tsv >t/ap24
+
+# Remove rem from ap24
+grep -v -f t/rem t/ap24 >t/pc25
+
+# Add the adds for pc24
+cat rep-num.txt >>t/pc25
+cut -f1 rep-add.tsv >>t/pc25
+
+# Get the unicode data for pc25 rep
+grep -f t/pc25 /home/oracc/pcsl/02pub/unicode.tsv >t/pc25.tsv
+
+# sort it
+sort -o t/pc25.tsv -k4n t/pc25.tsv
+
+# extract OID list for pc25 pages
+cut -f3 t/pc25.tsv >rep-pc25.txt
