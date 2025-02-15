a5c62b5df19f20b61df3c5c48b89daf9b7ea9179
Steve Tinney <stinney@upenn.edu>
2025-02-15 20:17:57 +0000

    work on cross-validation of corpus/pc25

--- 00etc/pc25/b/newrep.sh
+++ 00etc/pc25/b/newrep.sh
@@ -28,3 +28,10 @@ sort -o t/pc25.tsv -k4n t/pc25.tsv
 
 # extract OID list for pc25 pages
 cut -f3 t/pc25.tsv >rep-pc25.txt
+
+## Also build a list of PCSL signs which are not intended to be in PC25
+cat rm-{broken,opaque,seq}.txt >t/pc25.not
+cut -f1 rm-unattest.tsv >>t/pc25.not
+grep sequence pc25-tc-idg.tsv >>t/pc25.not
+cut -f1 ../acn-repertoire.tsv >>t/pc25.not
+sort -u t/pc25.not >rep-not.txt
