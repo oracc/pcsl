8a6d3077465a82db9a31f4da5e81b2f176fd2168
Steve Tinney <stinney@upenn.edu>
2025-07-09 11:37:03 -0400

    depc update draft proposal pdf

--- depc/00bin/attach-udata.sh
+++ depc/00bin/attach-udata.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 if [ -r depc.pdf ]; then
-    pdftk depc.pdf attach_files ../repc/UnicodeData.txt ../fepc/pc25-cc.ttf output pc25-proposal-202507.pdf
+    pdftk depc.pdf attach_files ../repc/UnicodeData.txt ../fepc/pc25-cc.ttf output pc25-draft-proposal-202507.pdf
 fi
