5cd8c36440228f37ea89c01fa49b89b3a8806a01
Steve Tinney <stinney@upenn.edu>
2025-08-11 21:20:41 -0400

    Appendix A char size fixing for font and cdli-gh

--- depc/00bin/attach-udata.sh
+++ depc/00bin/attach-udata.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 if [ -r depc.pdf ]; then
-    pdftk depc.pdf attach_files ../repc/UnicodeData.txt ../fepc/pc25-cc.ttf output pc25-draft-proposal-202507.pdf
+    date=`isogmt | tr -d -`
+    pdftk depc.pdf attach_files ../repc/UnicodeData.txt ../fepc/pc25-cc.ttf \
+	  output pc25-draft-proposal-$date.pdf
 fi
