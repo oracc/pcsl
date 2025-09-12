28801d8861d0726ef2fb2f076dd5dcf5ebe43dc8
Steve Tinney <stinney@upenn.edu>
2025-09-11 21:26:46 -0400

    misc updates towards non-draft version of proposal

--- mepc/lists/00bin/8reglyf.sh
+++ mepc/lists/00bin/8reglyf.sh
@@ -3,7 +3,6 @@ make clean base core csldist pc25
 rm -f 00lib/pcsl.asl
 make pcsl html
 make 00etc/seqdb.xml
-(cd ../texts ; ./b/pc25not.sh ; ./b/nothtml.sh)
+(cd ../texts ; ./00bin/pc25not.sh ; ./00bin/nothtml.sh)
 (cd ../../repc ; make)
 #(cd ../../fepc ; make)
-
